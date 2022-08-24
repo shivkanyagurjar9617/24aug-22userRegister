b3b01d2f03d7fcce43d8b3071ba446eee122187e
shivkanyagurjar9617 <shivkanyagurjar892@gmail.com>
2022-08-24 17:11:45 +0530

    done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,16 @@
 const { User } = require("../models/userModel");
-
+const bcrypt = require("bcrypt");
 let userController = (req,res)=>{
     console.log(req.body)
 
     User.findOne({email: req.body.email},function(error, user){
 
         if(user===null){
+
+            let saltRounds = 10;
+            let encPass = bcrypt.hashSync(req.body.password, saltRounds);
+            req.body.password = encPass
             const userObj = new User(req.body);
-    
             userObj.save().then(d =>{
                 res.status(200).json({
                     msg: "User saved successfully"
