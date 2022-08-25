7118fa288d48aa81cb834cc72d05e4cc41336043
shivkanyagurjar9617 <shivkanyagurjar892@gmail.com>
2022-08-25 11:17:24 +0530

    schema

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.x2agz.mongodb.net/?retryWrites=true&w=majority`)
+//mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.x2agz.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect('mongodb://localhost:27017')
 .then((d)=>console.log('connected'))
 .catch((e)=>console.log('error'))
 
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,12 +1,13 @@
+const { Schema } = require("mongoose");
 const { mongoose } = require("../config/dataBase");
 
 
-let userD = {
+let userSchema = new Schema({
     name:String,
     surname:String,
     email:String,
     password:String,
     userName:String
-}
-const User = mongoose.model('User', userD);
+})
+const User = mongoose.model('User', userSchema);
 module.exports = {User}
