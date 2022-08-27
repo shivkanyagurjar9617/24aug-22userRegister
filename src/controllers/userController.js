1b2cb9f5640f75b39c6d3968169305eaae025026
shivkanyagurjar9617 <shivkanyagurjar892@gmail.com>
2022-08-27 11:03:58 +0530

    27aug

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,8 @@
 const { User } = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+
+
 let userController = (req,res)=>{
     console.log(req.body)
 
@@ -12,9 +15,26 @@ let userController = (req,res)=>{
             req.body.password = encPass
             const userObj = new User(req.body);
             userObj.save().then(d =>{
+            const token = jwt.sign(req.body, process.env.JWT_TOKEN_KEY);
+               
+                let userDetail = {
+                    name:req.body.name,
+                    surname:req.body.surname,
+                    username:req.body.username,
+                    email:req.body.email,
+                    password:req.body.passwordname,
+                    usernames:req.body.username,
+                    role:req.body.role,
+                    token:token
+
+                }
+                delete userDetail.password
+                delete userDetail.username
                 res.status(200).json({
-                    msg: "User saved successfully"
+                    msg: "User saved successfully",
+                    userDetail:userDetail
                 })
+                
                 
             }).catch(e=>{
                 res.status(404).json({
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,11 +3,34 @@ const { mongoose } = require("../config/dataBase");
 
 
 let userSchema = new Schema({
-    name:String,
-    surname:String,
-    email:String,
-    password:String,
-    userName:String
+    name:{
+        type:String,
+        required:true
+    },
+    surname:{
+        type:String,
+        required:true,
+        description:'plese enter these field'
+    },
+    email:{
+        type:String,
+        required:true,
+        description:'plese enter email field'
+    },
+    password:{
+        type:String,
+        required:true,
+        description:'plese enter password field'
+    },
+    userName:{
+        type:String,
+        required:true,
+        description:'plese enter username field'
+    },
+    role:String,
+    
+},{
+    timestamps:true,
 })
 const User = mongoose.model('User', userSchema);
 module.exports = {User}
